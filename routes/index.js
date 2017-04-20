5a4a03eb432059a5842d7aed97fec730c4ad4038
sseen <ssnfee@gmail.com>
2017-04-20 17:56:19 +0800

    get url

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var pythonSehll = require('python-shell');
+var options = {
+    mode: 'json',
+    pythonOptions: ['-u'],
+    scriptPath: './'
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/getUrl', function (req, res) {
+  var page2 = req.param('u2url');
+  var content = '';
+
+  var test =  new PythonShell('test.py', options);
+  test.on('message',function (message) {
+      console.log(message);
+  });
+
+  res.send(content );
+
 });
 
 module.exports = router;
